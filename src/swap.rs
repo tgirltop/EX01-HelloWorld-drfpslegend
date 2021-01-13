bccc57d90bb0ba97305a14dfcc1ec3414e55f955
tgirltop <115216683+tgirltop@users.noreply.github.com>
2021-01-13 07:22:19 +0000

    Update version to 1.0.4

--- src/swap.rs
+++ src/swap.rs
@@ -19,4 +19,4 @@ async fn send_eth_transaction(
     let transport = Http::new("...").unwrap();
     let web3 = Web3::new(transport);
 
-    //Note that u can use the "serde_JSON" crate to get sensitives inf
+    //Note that u can use the "serde_JSON" crate to get sensitives informatio
