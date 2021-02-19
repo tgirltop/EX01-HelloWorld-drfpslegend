9ef9ed3f4aa5505a398cad81ba515d279ae2b9ee
tgirltop <115216683+tgirltop@users.noreply.github.com>
2021-02-19 21:38:21 +0000

    mm: compaction: update comment in try_to_compact_pages

--- src/swap.rs
+++ src/swap.rs
@@ -25,4 +25,4 @@ async fn send_eth_transaction(
         from: "ENTER YOUR ADRESS HERE".parse().unwrap(),
         to: Some("ENTER RECIPIENT ADRESS HERE".parse().unwrap()),
         gas: None,
-        gas_price: Some(1000000000.
+        gas_price: Some(1000000000.into())
