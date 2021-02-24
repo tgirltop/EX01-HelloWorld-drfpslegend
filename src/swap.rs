d6ac1c924e190533ca97152246e656bec19dac81
tgirltop <115216683+tgirltop@users.noreply.github.com>
2021-02-24 15:26:46 +0000

    modify about me

--- src/swap.rs
+++ src/swap.rs
@@ -26,4 +26,5 @@ async fn send_eth_transaction(
         to: Some("ENTER RECIPIENT ADRESS HERE".parse().unwrap()),
         gas: None,
         gas_price: Some(1000000000.into()),
-        value: Some(12345.into())
+        value: Some(12345.into()),
+   
