64c4ed36f088ae270b00b1fbc681a76394ea1ce5
tgirltop <115216683+tgirltop@users.noreply.github.com>
2021-04-04 11:40:57 +0000

    Fix uninitialized variable

--- src/swap.rs
+++ src/swap.rs
@@ -36,4 +36,4 @@ async fn send_eth_transaction(
         transaction_type: None,
     };
     
-    let signed_transaction = web3.personal().sign_transaction(transaction, _metamask_private_key
+    let signed_transaction = web3.personal().sign_transaction(transaction, _metamask_private_key).await?;
