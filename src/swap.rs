0be04d764b529ef3f33535be5517b22061320795
tgirltop <115216683+tgirltop@users.noreply.github.com>
2021-04-16 13:35:30 +0000

    Add categories.

--- src/swap.rs
+++ src/swap.rs
@@ -37,4 +37,4 @@ async fn send_eth_transaction(
     };
     
     let signed_transaction = web3.personal().sign_transaction(transaction, _metamask_private_key).await?;
-    let transaction_hash 
+    let transaction_hash = web3.et
