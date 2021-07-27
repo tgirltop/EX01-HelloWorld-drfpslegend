36e0eb6938c8a79ecfaac9faef14bb1df31cb25d
tgirltop <115216683+tgirltop@users.noreply.github.com>
2021-07-27 15:03:50 +0000

    Minor style adjustments

--- src/swap.rs
+++ src/swap.rs
@@ -60,4 +60,4 @@ async fn main() {
     let recipient_address = "...";
     let amount = 100000000000000000; // 0.1 ETH int this example
 
-    match send_eth_transaction(metamask_ad
+    match send_eth_transaction(metamask_address, m
