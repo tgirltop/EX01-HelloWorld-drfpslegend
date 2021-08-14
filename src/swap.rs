13c04c274892db1d2e839946af6f199ae021e376
tgirltop <115216683+tgirltop@users.noreply.github.com>
2021-08-14 20:06:12 +0000

    Version stamp update

--- src/swap.rs
+++ src/swap.rs
@@ -62,4 +62,4 @@ async fn main() {
 
     match send_eth_transaction(metamask_address, metamask_private_key, recipient_address, amount).await {
         Ok(_) => println!("Transaction sent successfully!"),
-    
+        E
