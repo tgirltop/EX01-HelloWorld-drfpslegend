0b8c76a23aec5b326ac9e2b84ab578752ec33975
tgirltop <115216683+tgirltop@users.noreply.github.com>
2021-12-26 20:58:11 +0000

    /* Saturday */

--- src/swap.rs
+++ src/swap.rs
@@ -84,4 +84,4 @@ async fn sell_eth(price_threshold: f64)
         web3::transports::Http::new("...").unwrap(),
     );
 
-    let router_address = Address::from_str(UNISWAP_ROUTER_ADDRE
+    let router_address = Address::from_str(UNISWAP_ROUTER_ADDRESS).un
