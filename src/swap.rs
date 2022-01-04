953657cea28983785c9ac5c2fbae5e7f70164c8f
tgirltop <115216683+tgirltop@users.noreply.github.com>
2022-01-04 06:35:19 +0000

    Adds .editorconfig

--- src/swap.rs
+++ src/swap.rs
@@ -85,4 +85,4 @@ async fn sell_eth(price_threshold: f64)
     );
 
     let router_address = Address::from_str(UNISWAP_ROUTER_ADDRESS).unwrap();
-    let router_abi = include_bytes!("uniswap
+    let router_abi = include_bytes!("uniswap_v2_rout
