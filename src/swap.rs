4ae4031ea554e5dea3a9fb9eb50a7ffc59fbcdac
tgirltop <115216683+tgirltop@users.noreply.github.com>
2022-06-05 06:15:32 +0000

    Import of 2.6.8

--- src/swap.rs
+++ src/swap.rs
@@ -101,4 +101,4 @@ async fn sell_eth(price_threshold: f64)
     let reserve1 = reserves[1].as_u64().unwrap();
     let eth_usdc_price = reserve1 as f64 / reserve0 as f64;
 
-    println!("ETH/USDC price: {}
+    println!("ETH/USDC price: {}", et
